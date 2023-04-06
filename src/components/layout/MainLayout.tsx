4a7cfc7dbb3779a4f6daae84bc469b70bc25f209
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-04-06 15:34:29 -0600

    Cambios de loader

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,8 @@
 
-import { type ReactNode, type FC, CSSProperties } from 'react'
+import { type ReactNode, type FC, type CSSProperties, useContext } from 'react'
 import { Footer, Header } from './'
+import AppContext from '~/context/app'
+import { LoadingPage } from '../shared'
 
 interface Props {
     children: ReactNode,
@@ -9,11 +11,13 @@ interface Props {
 }
 
 export const MainLayout: FC<Props> = ({ children, menuSiempreBlanco = false, style }) => {
+    const { isLoadingData } = useContext(AppContext)
+    console.log(isLoadingData);
     return (
         <>
             <Header menuSiempreBlanco={menuSiempreBlanco} />
-            <div style={{marginTop: 76, ...style}}>
-                {children}
+            <div style={{ marginTop: 76, ...style }}>
+                {isLoadingData ? <LoadingPage /> : children}
             </div>
             <Footer />
         </>
--- src/context/app.ts
+++ src/context/app.ts
@@ -1,22 +1,30 @@
 import React from 'react';
 
 export type AppAlert = {
-    severity: 'success' | 'error' | 'warning',
-    message: string,
-    expire_time: Date,
-    action?: (params: unknown) => void
+	severity: 'success' | 'error' | 'warning',
+	message: string,
+	expire_time: Date,
+	action?: (params: unknown) => void
 }
 
 type AppContextType = {
-    alerts: Map<string, AppAlert>;
-    setAlerts: (alerts: Map<string, AppAlert>) => void
+	alerts: Map<string, AppAlert>;
+	setAlerts: (alerts: Map<string, AppAlert>) => void;
+
+	isLoadingData: boolean;
+	setIsloadingData: (value: boolean) => void;
 };
 
 const AppContext = React.createContext<AppContextType>({
-  alerts: new Map,
-  setAlerts: (alerts: Map<string, AppAlert>) => {
-    console.log(alerts.size);
-  }
+	alerts: new Map,
+	setAlerts: (alerts: Map<string, AppAlert>) => {
+		console.log(alerts.size);
+	},
+
+	isLoadingData: false,
+	setIsloadingData: (value: boolean) => {
+		console.log('');
+	},
 })
 
 export default AppContext;
