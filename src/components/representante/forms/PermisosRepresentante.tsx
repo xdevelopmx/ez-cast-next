6b4c64e56153a98f5c5ad678c6555e5c46a4c85b
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-04-21 14:27:28 -0600

    Paso 2 del formulario de representante

--- src/components/representante/forms/PermisosRepresentante.tsx
+++ src/components/representante/forms/PermisosRepresentante.tsx
@@ -23,12 +23,42 @@ export const PermisosRepresentante = () => {
                         }
                         console.log('change'); */
                     }}
-                    id="tipos-apariencias-rol"
+                    id="quien-puede-aceptar-solicitudes-select"
                     labelStyle={{ marginBottom: 0, width: '45%' }}
-                    options={/* (tipos_nsfw.data) ? tipos_nsfw.data.map(n => n.es) : */ []}
+                    options={/* (tipos_nsfw.data) ? tipos_nsfw.data.map(n => n.es) : */
+                        ['Talentos', 'Representante']
+                    }
                     values={/* (tipos_nsfw.data) ? tipos_nsfw.data.map(g => {
                         return state.nsfw.ids.includes(g.id);
-                    }) : */ [false]}
+                    }) : */ [false, false]}
+                />
+            </Grid>
+            <Grid xs={12} mt={4}>
+                <MCheckboxGroup
+                    direction='vertical'
+                    title="¿Quién puede editar perfil de talento?"
+                    onChange={(e, i) => {
+                        /* const nsfw = tipos_nsfw.data?.filter((_, index) => index === i)[0];
+                        if (nsfw) {
+                            onFormChange({
+                                nsfw: {
+                                    ...state.nsfw,
+                                    ids: (state.nsfw.ids.includes(nsfw.id)) ?
+                                        state.nsfw.ids.filter(n => n !== nsfw.id) :
+                                        state.nsfw.ids.concat([nsfw.id])
+                                }
+                            })
+                        }
+                        console.log('change'); */
+                    }}
+                    id="quien-puede-aceptar-solicitudes-select"
+                    labelStyle={{ marginBottom: 0, width: '45%' }}
+                    options={/* (tipos_nsfw.data) ? tipos_nsfw.data.map(n => n.es) : */
+                        ['Talentos', 'Representante']
+                    }
+                    values={/* (tipos_nsfw.data) ? tipos_nsfw.data.map(g => {
+                        return state.nsfw.ids.includes(g.id);
+                    }) : */ [false, false]}
                 />
             </Grid>
         </Grid>
