b63a9c7682e45fdd434d6b4143ad85a731c26986
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-04-26 14:53:53 -0600

    Cambios de archivos

--- src/pages/cazatalentos/agenda-virtual/index.tsx
+++ src/pages/cazatalentos/agenda-virtual/index.tsx
@@ -9,46 +9,46 @@ const AgendaVirtual = () => {
     return (
         <>
             <Head>
-				<title>Cazatalentos | Talent Corner</title>
-				<meta name="description" content="Talent Corner" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+                <title>Cazatalentos | Talent Corner</title>
+                <meta name="description" content="Talent Corner" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
             <MainLayout menuSiempreBlanco={true}>
                 <div className="d-flex wrapper_ezc">
-					<MenuLateral />
+                    <MenuLateral />
                     <div className="seccion_container col" style={{ paddingTop: 0 }}>
-						<br /><br />
-						<div className="container_box_header">
-							<div className="d-flex justify-content-end align-items-start py-2">
-								<Alertas />
-							</div>
-							<Grid container>
+                        <br /><br />
+                        <div className="container_box_header">
+                            <div className="d-flex justify-content-end align-items-start py-2">
+                                <Alertas />
+                            </div>
+                            <Grid container>
                                 <Grid item xs={12}>
                                     <Grid container item columns={12}>
                                         <Grid item md={1} textAlign={'center'}>
-                                            <Image src="/assets/img/iconos/agenda.svg" width={50} height={50} style={{margin: '15px 0 0 0', filter: 'invert(43%) sepia(92%) saturate(431%) hue-rotate(140deg) brightness(97%) contrast(101%)'}} alt="" />
+                                            <Image src="/assets/img/iconos/agenda.svg" width={50} height={50} style={{ margin: '15px 0 0 0', filter: 'invert(43%) sepia(92%) saturate(431%) hue-rotate(140deg) brightness(97%) contrast(101%)' }} alt="" />
                                         </Grid>
                                         <Grid item md={11}>
                                             <Typography fontWeight={800} sx={{ color: '#069cb1', fontSize: '2rem' }}>Agenda Virtual</Typography>
                                             <Typography fontWeight={600} sx={{ color: '#000', fontSize: '`.7rem' }}>Horarios</Typography>
                                         </Grid>
-                                        
+
                                     </Grid>
-								</Grid>
-                                <div style={{textAlign: 'right', width: '100%'}}>
-                                    <Link href="/cazatalentos/agenda-virtual/crear" className="btn btn-intro btn-price mb-2 mt-3"><Image src="/assets/img/iconos/mas.svg" width={14} height={14} style={{filter : 'invert(1)', margin: '0 0 2px 0'}} alt="" />&nbsp;&nbsp;Nuevo horario</Link>
+                                </Grid>
+                                <div style={{ textAlign: 'right', width: '100%' }}>
+                                    <Link href="/cazatalentos/agenda-virtual/crear" className="btn btn-intro btn-price mb-2 mt-3"><Image src="/assets/img/iconos/mas.svg" width={14} height={14} style={{ filter: 'invert(1)', margin: '0 0 2px 0' }} alt="" />&nbsp;&nbsp;Nuevo horario</Link>
                                 </div>
-								<Grid item xs={12}>
+                                <Grid item xs={12}>
                                     <Grid container item xs={20} sx={{ backgroundColor: '#fff', padding: '10px 10px' }} columns={18}>
                                         <Grid item md={4} textAlign={'center'}>
-                                             
+
                                         </Grid>
                                         <Grid item md={1} textAlign={'center'}>
-                                             
+
                                         </Grid>
                                         <Grid item md={4} textAlign={'center'}>
-                                            
+
                                         </Grid>
                                         <Grid item md={2} textAlign={'center'}>
                                             Pendientes
@@ -63,7 +63,7 @@ const AgendaVirtual = () => {
                                             Reagendados
                                         </Grid>
                                         <Grid item md={1} textAlign={'center'}>
-                                            
+
                                         </Grid>
                                     </Grid>
                                     <Grid container item xs={18} sx={{ backgroundColor: '#069cb1', padding: '16px 10px', margin: '0 0 16px 0' }} columns={18}>
@@ -77,19 +77,19 @@ const AgendaVirtual = () => {
                                             Fecha de creación
                                         </Grid>
                                         <Grid item md={2} textAlign={'center'}>
-                                            <Image src="/assets/img/iconos/relojdearena.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/relojdearena.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                         <Grid item md={2} textAlign={'center'}>
-                                            <Image src="/assets/img/iconos/check-k.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/check-k.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                         <Grid item md={2} textAlign={'center'}>
-                                            <Image src="/assets/img/iconos/tache.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/tache.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                         <Grid item md={2} textAlign={'center'}>
-                                            <Image src="/assets/img/iconos/reloj.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/reloj.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                         <Grid item md={1} textAlign={'right'}>
-                                            
+
                                         </Grid>
                                     </Grid>
                                     <Grid container item xs={20} sx={{ backgroundColor: '#069cb185', padding: '5px 10px', margin: '4px 0' }} columns={18}>
@@ -115,7 +115,7 @@ const AgendaVirtual = () => {
                                             2
                                         </Grid>
                                         <Grid item md={1} textAlign={'right'}>
-                                            <Image src="/assets/img/iconos/kebab-menu-k.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/kebab-menu-k.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                     </Grid>
                                     <Grid container item xs={20} sx={{ backgroundColor: '#ea9d2185', padding: '5px 10px', margin: '4px 0' }} columns={18}>
@@ -141,13 +141,17 @@ const AgendaVirtual = () => {
                                             2
                                         </Grid>
                                         <Grid item md={1} textAlign={'right'}>
-                                            <Image src="/assets/img/iconos/kebab-menu-k.svg" width={20} height={20} style={{filter : 'invert(1)'}} alt="" />
+                                            <Image src="/assets/img/iconos/kebab-menu-k.svg" width={20} height={20} style={{ filter: 'invert(1)' }} alt="" />
                                         </Grid>
                                     </Grid>
-								</Grid>
-							</Grid>
-						</div>
-					</div>
+
+                                    <Grid xs={12}>
+                                        
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
                 </div>
             </MainLayout>
         </>
